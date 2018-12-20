61d169df6ffc6c496787867197e686dd04b32053
Chris West (Faux) <git@goeswhere.com>
2018-12-20 09:10:14 +0000

    also test st

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('@snyk/nodejs-runtime-agent')({
     projectId: '8a2be5c4-797f-44d6-afac-4e7394f6aa1b',
     beaconIntervalMs: 2000,
+    snapshotUrl: 'http://no/'
 });
 
 const cryptiles = require('cryptiles');
+const st = require('st');
 
 for (var i = 0; i < 10; ++i) {
     setTimeout(() => {
+        try {
+            st.Mount.prototype.getPath();
+        } catch (err) {
+            console.log(err);
+        }
         console.log(cryptiles.randomDigits(5));
     }, i * 1000);
 }
